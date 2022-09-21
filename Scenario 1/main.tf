3e608f0a56c234b42be75fd3d99b3b652c18095a
simil.mittal <simil.mittal@algosec.com>
2022-09-21 17:14:19 +0530

    git push

---
+++ Scenario 1/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+
+  access_key = "AKIAXN5RHA6VRMJWNLMR"
+  secret_key = "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"
+  region     = "us-west-1"
+}
+
+
+
+resource "aws_security_group" "Scenario1" {
+  name        = "SG with all port and all IP."
+  description = "Ingress"
+  vpc_id      = "vpc-481b202f"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Scenario1"
+  }
+}
+
+
+
