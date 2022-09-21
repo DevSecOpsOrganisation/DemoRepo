b8ac347bc2066f14d7f25903e7b0812ef139d51c
simil.mittal <simil.mittal@algosec.com>
2022-09-21 17:27:53 +0530

    New scenario

--- Scenario 2/main.tf
+++ Scenario 2/main.tf
@@ -6,14 +6,21 @@ provider "aws" {
 }
 
 resource "aws_security_group" "Scenario2" {
-  name        = "SG with Single port and all IP."
+  name        = "SG with Single port(UDP and TCP) and all IP."
   description = "Ingress"
   vpc_id      = "vpc-481b202f"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 389
+    to_port     = 389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 389
+    to_port     = 389
+    protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
