2f3f34077cb02ec4af332e36d30fd0b47bc655ae
simil.mittal <simil.mittal@algosec.com>
2022-09-22 12:50:13 +0530

    correct chnages

--- Scenario 5/main.tf
+++ Scenario 5/main.tf
@@ -19,17 +19,17 @@ resource "aws_security_group" "Scenario5" {
       "192.168.0.0/16"
     ]
   }
-
-
   ingress {
-    description = "All ICMP - IPv4"
-    from_port   = -1
-    to_port     = -1
-    protocol    = "ICMP"
-    cidr_blocks = ["0.0.0.0/0"]
-
+    from_port   = 22
+    to_port     = 22
+    protocol    = "udp"
+    cidr_blocks = [
+      "192.168.1.1/32",
+      "192.168.1.2/32",
+      "192.168.1.3/32",
+      "192.168.0.0/16"
+    ]
   }
-
 
   tags = {
     Name = "Scenario5"
