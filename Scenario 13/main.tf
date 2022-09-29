f2b23efabe0e28e0f447ad03ec2d2b93690af5c0
simil.mittal <simil.mittal@algosec.com>
2022-09-29 11:39:00 +0530

    Scenario 13 and 14

--- Scenario 13/main.tf
+++ Scenario 13/main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "us-west-1"
+  access_key = "AKIAXN5RHA6VXOYRSJ6D"
+  secret_key = "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"
 }
 
 resource "aws_security_group" "Scenario13" {
@@ -8,8 +10,19 @@ resource "aws_security_group" "Scenario13" {
   vpc_id      = "vpc-481b202f"
 
   egress {
-    from_port   = -1
-    to_port     = -1
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [
+      "192.168.1.1/32",
+      "192.168.1.2/32",
+      "192.168.1.3/32",
+      "4.4.4.0/24"
+    ]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = [
       "192.168.1.1/32",
@@ -19,8 +32,8 @@ resource "aws_security_group" "Scenario13" {
     ]
   }
   egress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 0
+    to_port     = 0
     protocol    = "udp"
     cidr_blocks = [
       "192.168.1.1/32",
@@ -30,6 +43,6 @@ resource "aws_security_group" "Scenario13" {
     ]
   }
   tags = {
-    Name = "SG with Single port(UDP and TCP) and Multiple IP"
+    Name = "SG with Single port(UDP, TCP and all) and Multiple IP"
   }
 }
