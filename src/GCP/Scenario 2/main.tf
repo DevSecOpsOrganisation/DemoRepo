4b5b0ff15d0280b330dce09b3ab1aeb38eb4e30d
simil.mittal <simil.mittal@algosec.com>
2022-10-20 12:44:47 +0530

    GCP

---
+++ src/GCP/Scenario 2/main.tf
@@ -0,0 +1,21 @@
+resource "google_compute_firewall" "i02-ni-gcp" {
+  name = "devsecops-fw-single-port"
+  network = "devsecops-network-gcp"
+
+  allow {
+    protocol = "tcp"
+    ports = [
+      "389"]
+  }
+  allow {
+    protocol = "udp"
+    ports = [
+      "389"]
+  }
+
+  description = "INGRESS"
+  direction = "INGRESS"
+  priority = 1100
+  source_ranges = [
+    "0.0.0.0/0"]
+}
