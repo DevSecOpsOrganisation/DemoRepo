ab40cab7fd845a52bec01a98b7350013a48fd7a3
simil.mittal <simil.mittal@algosec.com>
2022-10-28 12:20:49 +0530

    Added terraform in main folder

---
+++ mail.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region     = "us-west-1"
+}
+
+resource "aws_security_group" "Scenario17" {
+  name        = "SG with Single port(UDP and TCP) and all IP. "
+  description = "Ingress"
+  vpc_id      = "vpc-481b202f"
+
+  ingress {
+    from_port   = 389
+    to_port     = 389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 389
+    to_port     = 389
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Scenario17"
+  }
+}
+
+
+
