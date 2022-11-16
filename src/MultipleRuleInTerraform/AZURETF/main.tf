16cc1b8930ba390abd0ef59a9922f818a07dd091
DevSecOpsAutomation <115538844+DevSecOpsAutomation@users.noreply.github.com>
2022-11-16 13:51:20 +0530

    Azure files added

---
+++ src/MultipleRuleInTerraform/AZURETF/main.tf
@@ -0,0 +1,53 @@
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_network_security_group" "Scenario1" {
+  name                = "Scenario1"
+  location            = "East US"
+  resource_group_name = "devsecops-resources"
+  security_rule {
+    name                       = "Scenario1"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name = "AllowSingleSourcePort_Inbound"
+    priority = 110
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "389"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name = "Scenario3_udp"
+    priority = 130
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Udp"
+    source_port_range = "*"
+    destination_port_range = "3300-3400"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "Scenario11"
+    priority                   = 210
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
