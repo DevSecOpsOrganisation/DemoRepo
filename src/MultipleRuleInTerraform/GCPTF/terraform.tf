08c48b721c112ac9fab456c7dda5a3e51d8bed83
DevSecOpsAutomation <115538844+DevSecOpsAutomation@users.noreply.github.com>
2022-11-18 12:43:46 +0530

    New structure

--- src/MultipleRuleInTerraform/GCPTF/terraform.tf
+++ src/MultipleRuleInTerraform/GCPTF/terraform.tf
@@ -3,34 +3,40 @@ resource "google_compute_firewall" "i02-ni-gcp" {
   network = "devsecops-network-gcp"
 
   allow {
-    protocol = var.tcp_protocol
+    protocol = "tcp"
     ports    = [
       "389"
     ]
   }
   allow {
-    protocol = var.udp_protocol
+    protocol = "udp"
     ports    = [
       "380"
     ]
   }
 
   allow {
-    protocol = var.tcp_protocol
+    protocol = "tcp"
     ports    = [
       "3020-3050",
       "9000"
     ]
   }
   allow {
-    protocol = var.udp_protocol
+    protocol = "udp"
     ports    = [
       "3020-3050",
       "9000"
     ]
   }
+
+
   description   = "INGRESS"
   direction     = "INGRESS"
   priority      = 1100
-  source_ranges = var.all_ips
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+
+
 }
