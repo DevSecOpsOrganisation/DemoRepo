c3eea236959f5b751befdc2c78c13da3143a9bbb
DevSecOpsAutomation <115538844+DevSecOpsAutomation@users.noreply.github.com>
2022-11-18 12:27:31 +0530

    New structure

--- src/RulesWithoutRisk/main.tf
+++ src/RulesWithoutRisk/main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-  region = "us-west-1"
+  region = var.region
 }
 
 resource "aws_security_group" "Scenario8" {
   name        = "SG without risk"
   description = "Ingress"
-  vpc_id      = "vpc-481b202f"
+  vpc_id      = var.vpc_id
 
   ingress {
     description = "SSH"
     from_port   = 32
     to_port     = 33
-    protocol    = "tcp"
-    cidr_blocks = ["1.1.1.1/32"]
+    protocol    = var.tcp_protocol
+    cidr_blocks = var.private_ip
 
   }
 
