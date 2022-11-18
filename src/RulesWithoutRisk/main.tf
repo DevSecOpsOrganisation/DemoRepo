e8e49cba68375b8ce064a799894b33d4067ff9d4
DevSecOpsAutomation <115538844+DevSecOpsAutomation@users.noreply.github.com>
2022-11-18 12:31:29 +0530

    New structure

--- src/RulesWithoutRisk/main.tf
+++ src/RulesWithoutRisk/main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "Scenario8" {
     description = "SSH"
     from_port   = 32
     to_port     = 33
-    protocol    = var.tcp_protocol
+    protocol    = var.all_protocol
     cidr_blocks = var.private_ip
 
   }
