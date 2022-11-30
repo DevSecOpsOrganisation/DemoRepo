3157d661bf6ad1214f3a2e4468eb6a4012a10e33
anatkl <100366908+anatkl@users.noreply.github.com>
2022-11-30 16:39:31 +0200

    Update main.tf

--- src/RulesWithoutRisk/main.tf
+++ src/RulesWithoutRisk/main.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "Scenario8" {
     from_port   = 32
     to_port     = 33
     protocol    = var.tcp_protocol
-    cidr_blocks = var.private_ip
+    cidr_blocks = var.public_ip
 
   }
 
