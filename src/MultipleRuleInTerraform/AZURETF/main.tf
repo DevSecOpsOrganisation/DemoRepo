33dc43e5347c27dd3bd073ee75cbeaebebf06481
anatkl <100366908+anatkl@users.noreply.github.com>
2023-05-16 15:11:43 +0300

    Update main.tf

--- src/MultipleRuleInTerraform/AZURETF/main.tf
+++ src/MultipleRuleInTerraform/AZURETF/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 resource "azurerm_network_security_group" "Scenario1" {
   name                = "Scenario1"
   location            = "East US"
-  resource_group_name = "devsecops-resources"
+  resource_group_name = "devsecops--resources"
   security_rule {
     name                       = "Scenario1"
     priority                   = 110
