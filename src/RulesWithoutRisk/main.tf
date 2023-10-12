dbd24029172dcf709fe6156c482f27bbe030bc20
similAlgo <111849250+similAlgo@users.noreply.github.com>
2023-10-12 14:19:29 +0530

    Update main.tf

--- src/RulesWithoutRisk/main.tf
+++ src/RulesWithoutRisk/main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "Scenario8" {
     description = "SSH"
     from_port   = 0
     to_port     = 65535
-    protocol    = var.udp_protocol
+    protocol    = var.all_protocol
     cidr_blocks = var.private_ip
   }
 
