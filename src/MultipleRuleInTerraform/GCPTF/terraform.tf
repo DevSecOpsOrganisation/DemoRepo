114cf91fcd481283866171f0a63f3e217fc50056
similAlgo <111849250+similAlgo@users.noreply.github.com>
2023-10-12 16:39:11 +0530

    Update terraform.tf

--- src/MultipleRuleInTerraform/GCPTF/terraform.tf
+++ src/MultipleRuleInTerraform/GCPTF/terraform.tf
@@ -1,3 +1,7 @@
+provider "google"{
+  project = "cloudflow-qa-gcp-1"
+}
+
 resource "google_compute_firewall" "i02-ni-gcp" {
   name    = "devsecops-fw-single-port"
   network = "devsecops-network-gcp"
