e0debf9881bcbe0d7f8063106077158530f1aec5
anatkl <100366908+anatkl@users.noreply.github.com>
2023-11-13 11:13:19 +0200

    Update main.tf

--- src/MultipleRuleInTerraform/AWSTF/main.tf
+++ src/MultipleRuleInTerraform/AWSTF/main.tf
@@ -8,8 +8,8 @@ resource "aws_security_group" "Scenario111" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 0
-    to_port     = 65535
+    from_port   = 389
+    to_port     = 389
     protocol    = var.all_protocol
     cidr_blocks = var.all_ips
   }
