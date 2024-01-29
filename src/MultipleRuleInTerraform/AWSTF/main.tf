fa5fe922b367a077fad8c9a6d5b9575c22cbff5f
LarsMoellerNielsen <119936289+LarsMoellerNielsen@users.noreply.github.com>
2024-01-29 14:22:04 +0100

    Update main.tf

--- src/MultipleRuleInTerraform/AWSTF/main.tf
+++ src/MultipleRuleInTerraform/AWSTF/main.tf
@@ -31,14 +31,14 @@ resource "aws_security_group" "Scenario111" {
     from_port   = 0
     to_port     = 65535
     protocol    = var.tcp_protocol
-    cidr_blocks = var.all_ips
+    cidr_blocks = var.private_ip
   }
 
   egress {
     from_port   = 0
     to_port     = 65535
     protocol    = var.udp_protocol
-    cidr_blocks = var.all_ips
+    cidr_blocks = var.private_ip
   }
 
   tags = {
